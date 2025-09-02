f8935fd451ab5ca96fd481c7ca63b86c7fa2ef82
Pi Timelapse <pi@pi-tl.local>
2025-09-02 10:40:05 +0100

    Initial timelapse web interface

--- app.py
+++ app.py
@@ -614,6 +614,12 @@ def schedule_cancel():
 # ================== /Simple Scheduler ==================
 
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", "5050"))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 # --- init scheduler blueprint (safe to run multiple times) ---
 try:
