8791d38fbefa2acfc968ebdee112aa895e446fbb
lduffy23 <lduffy23@p5m-j2y5l97v.home>
2025-09-05 17:32:52 +0100

    Fix for SPI issues and screen white-out crashes

--- lcd_hat.py
+++ lcd_hat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
 import os, sys, time, json, threading, traceback
 from datetime import datetime
 from urllib.request import urlopen, Request
