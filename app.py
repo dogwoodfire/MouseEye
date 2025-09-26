917d86031063be3e3809dafc5a2463ca71f9833e
dogwoodfire <liam.duffy@mac.com>
2025-09-26 09:16:42 +0100

    cleaning up footer

--- app.py
+++ app.py
@@ -2279,7 +2279,7 @@ TPL_INDEX = r"""
 
 <main>
 {% set ap = ap_status %}
-    <div class="card" id="ap-indicator" style="background:#e9f0f7;padding:0px 12px;">
+    <div class="card" id="ap-indicator" style="background:#e9f0f7;padding:5px 12px;">
       <div class="row" style="justify-content: space-between;">
         <span>
           {% if ap.on %}
